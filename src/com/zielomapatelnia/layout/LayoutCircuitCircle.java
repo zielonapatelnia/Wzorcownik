567616f4efe24e9a3849c9ce3d7802acc7a82415
Dev <dev@MacBook-Pro-Krzysztof.local>
2016-08-12 10:29:09 +0200

    refactor - LayoutCircuitParallelogram

--- src/com/zielomapatelnia/layout/LayoutCircuitCircle.java
+++ src/com/zielomapatelnia/layout/LayoutCircuitCircle.java
@@ -9,74 +9,55 @@ import javax.swing.JTextField;
 
 import com.zielonapatelnia.math.CalculationCircuitCircle;
 
-public class LayoutCircuitCircle implements ActionListener {
-
+public class LayoutCircuitCircle implements ActionListener 
+{
 	JLabel jlHeading, jlResult;
 	JButton jbCalculate;
 	JTextField jtfResult, jtfValue;
 	
 	public LayoutCircuitCircle()
 	{
-	
-	
-	
-	JFrame oknoMiObwódKoło = new JFrame();
-	oknoMiObwódKoło.setLayout(null);
-
-	
-	
-	oknoMiObwódKoło.setSize(400, 400);
-	oknoMiObwódKoło.setTitle("Obliczanie obwodu koła");
-	oknoMiObwódKoło.setDefaultCloseOperation(1);
-	oknoMiObwódKoło.setVisible(true);
+	JFrame windowMiCircuitCircle = new JFrame();
+	windowMiCircuitCircle.setLayout(null);
+	windowMiCircuitCircle.setSize(400, 400);
+	windowMiCircuitCircle.setTitle("Obliczanie obwodu koła");
+	windowMiCircuitCircle.setDefaultCloseOperation(1);
+	windowMiCircuitCircle.setVisible(true);
 	
 	jlHeading = new JLabel("Aby obliczyć obwód koła podaj średnicę koła w cm");
 	jlHeading.setBounds(70, 10, 300, 20);
-	oknoMiObwódKoło.add(jlHeading);	
+	windowMiCircuitCircle.add(jlHeading);	
 	
 	jtfValue = new JTextField();
 	jtfValue.setBounds(150, 60, 100, 40);
-	oknoMiObwódKoło.add(jtfValue);
-	
+	windowMiCircuitCircle.add(jtfValue);
 	
 	jbCalculate = new JButton("Oblicz !");
 	jbCalculate.setBounds(50, 150, 300, 40);
-	oknoMiObwódKoło.add(jbCalculate);
+	windowMiCircuitCircle.add(jbCalculate);
 	jbCalculate.addActionListener(this);
 	
 	jlResult = new JLabel("Wynik");
 	jlResult.setBounds(180, 210, 100, 20);
-	oknoMiObwódKoło.add(jlResult);
+	windowMiCircuitCircle.add(jlResult);
 	
 	jtfResult = new JTextField();
 	jtfResult.setBounds(150, 260, 100, 40);
-	oknoMiObwódKoło.add(jtfResult);
+	windowMiCircuitCircle.add(jtfResult);
 	}
 	
-	
-	
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e)
+	{
 		Object z = e.getSource();
 		if (z == jbCalculate)
 		{
 			double a = Double.parseDouble(jtfValue.getText());
 			
-			CalculationCircuitCircle rezultat = new CalculationCircuitCircle(a);
-			String wynik = rezultat.rozwiązanieObwódKoło();
-			jtfResult.setText(wynik);
-			
-			
-			
+			CalculationCircuitCircle result = new CalculationCircuitCircle(a);
+			String resultFinal = result.rozwiązanieObwódKoło();
+			jtfResult.setText(resultFinal);
 			
 		}
-		
-		
-		
 	}
-
-
-
-	
-
 }
--- src/com/zielomapatelnia/layout/LayoutCircuitParallelogram.java
+++ src/com/zielomapatelnia/layout/LayoutCircuitParallelogram.java
@@ -9,77 +9,63 @@ import javax.swing.JTextField;
 
 import com.zielonapatelnia.math.CalculationCircuitParallelogram;
 
-public class LayoutCircuitParallelogram implements ActionListener {
-	
-	JLabel jlNagłówek, jlWynik;
-	JButton jbOblicz;
-	JTextField jtfWynik, jtfWartość1, jtfWartość2;
+public class LayoutCircuitParallelogram implements ActionListener 
+{
+	JLabel jlHeading, jlResult;
+	JButton jbCalculate;
+	JTextField jtfResult, jtfValue1, jtfValue2;
 	
 	public LayoutCircuitParallelogram()
 	{
+		JFrame windowsMiCircuitParallelogram = new JFrame();
+		windowsMiCircuitParallelogram.setLayout(null);
+		windowsMiCircuitParallelogram.setSize(400, 400);
+		windowsMiCircuitParallelogram.setTitle("Obliczanie obwodu równoległoboku");
+		windowsMiCircuitParallelogram.setDefaultCloseOperation(1);
+		windowsMiCircuitParallelogram.setVisible(true);	
 		
-	
+		jlHeading = new JLabel("Aby obliczyć obwód równole©równoległoboku podaj długość boku a oraz b");
+		jlHeading.setBounds(20, 10, 400, 20);
+		windowsMiCircuitParallelogram.add(jlHeading);	
 		
-		JFrame oknoMiObwódRównoległobok = new JFrame();
-		oknoMiObwódRównoległobok.setLayout(null);
-		oknoMiObwódRównoległobok.setSize(400, 400);
-		oknoMiObwódRównoległobok.setTitle("Obliczanie obwodu równoległoboku");
-		oknoMiObwódRównoległobok.setDefaultCloseOperation(1);
-		oknoMiObwódRównoległobok.setVisible(true);	
+		jtfValue1 = new JTextField();
+		jtfValue1.setBounds(50, 60, 70, 40);
+		windowsMiCircuitParallelogram.add(jtfValue1);
 		
+		jtfValue2 = new JTextField();
+		jtfValue2.setBounds(150, 60, 70, 40);
+		windowsMiCircuitParallelogram.add(jtfValue2);
 		
-		jlNagłówek = new JLabel("Aby obliczyć obwód równole©równoległoboku podaj długość boku a oraz b");
-		jlNagłówek.setBounds(20, 10, 400, 20);
-		oknoMiObwódRównoległobok.add(jlNagłówek);	
+		jbCalculate = new JButton("Oblicz !");
+		jbCalculate.setBounds(50, 150, 300, 40);
+		windowsMiCircuitParallelogram.add(jbCalculate);
+		jbCalculate.addActionListener(this);
 		
-		jtfWartość1 = new JTextField();
-		jtfWartość1.setBounds(50, 60, 70, 40);
-		oknoMiObwódRównoległobok.add(jtfWartość1);
+		jlResult = new JLabel("Wynik");
+		jlResult.setBounds(180, 210, 100, 20);
+		windowsMiCircuitParallelogram.add(jlResult);
 		
-		jtfWartość2 = new JTextField();
-		jtfWartość2.setBounds(150, 60, 70, 40);
-		oknoMiObwódRównoległobok.add(jtfWartość2);
-		
-	
-		
-		jbOblicz = new JButton("Oblicz !");
-		jbOblicz.setBounds(50, 150, 300, 40);
-		oknoMiObwódRównoległobok.add(jbOblicz);
-		jbOblicz.addActionListener(this);
-		
-		jlWynik = new JLabel("Wynik");
-		jlWynik.setBounds(180, 210, 100, 20);
-		oknoMiObwódRównoległobok.add(jlWynik);
-		
-		jtfWynik = new JTextField();
-		jtfWynik.setBounds(150, 260, 100, 40);
-		oknoMiObwódRównoległobok.add(jtfWynik);
+		jtfResult = new JTextField();
+		jtfResult.setBounds(150, 260, 100, 40);
+		windowsMiCircuitParallelogram.add(jtfResult);
 	}
 	
-	
-
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
 		Object z = e.getSource();
 		
-		if (z == jbOblicz)
+		if (z == jbCalculate)
 		{
-			double a = Double.parseDouble(jtfWartość1.getText());
-			double b = Double.parseDouble(jtfWartość2.getText());
+			double a = Double.parseDouble(jtfValue1.getText());
+			double b = Double.parseDouble(jtfValue2.getText());
 			
-			CalculationCircuitParallelogram obiekt = new CalculationCircuitParallelogram(a,b);
-			String rozwiązanie = obiekt.rozwiązanieObwódRównoległoboku();
-			jtfWynik.setText(rozwiązanie);
+			CalculationCircuitParallelogram result = new CalculationCircuitParallelogram(a,b);
+			String resultFinal = result.rozwiązanieObwódRównoległoboku();
+			jtfResult.setText(resultFinal);
 			
 			
 			
 		}
-		
 	}
-
-
-
-
-
 }
